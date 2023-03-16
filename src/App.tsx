e155334841df22ed88868a3236b1b9d8f100a865
jakir-ruet <jakir.ruet.bd@gmail.com>
2023-03-16 10:36:55 +0600

    required packages installed

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 function App() {
   return (
     <div>
-      <h1>hello react</h1>
+      <h1 className="text-3xl font-bold underline bg-red-500">hello react</h1>
     </div>
   );
 }
